4c1642ca5cb6339ea10a8b668c302fda9703f77a
Matt Fischer <matt@mattfischer.com>
2016-04-21 21:32:55 -0600

    Mitaka support
    - Support for Mitaka
    - Remove unused/unsupported Assignment driver
    
    Change-Id: I0a1149e9bd0eae17fad4cd2e9be767c7ad55662b

--- keystone_hybrid_backend/hybrid_identity.py
+++ keystone_hybrid_backend/hybrid_identity.py
@@ -48,37 +48,37 @@ class Identity(sql_ident.Identity):
         """
         if not password:
             raise AssertionError('Invalid user / password')
-        session = sql.get_session()
-        try:
-            user_ref = self._get_user(session, user_id)
-        except exception.UserNotFound:
-            raise AssertionError('Invalid user / password')
-
-        # if the user_ref has a password, it's from the SQL backend and
-        # we can just check if it coincides with the one we got
-        conn = None
-        try:
-            assert utils.check_password(password, user_ref['password'])
-        except TypeError:
-            raise AssertionError('Invalid user / password')
-        except (KeyError, AssertionError):  # if it doesn't have a password, it must be LDAP
+        with sql.session_for_read() as session:
             try:
-                user_name = user_ref['name']
-                # get_connection does a bind for us which checks the password
-                conn = self.user.get_connection(self.user._id_to_dn(user_name),
-                                                password, end_user_auth=True)
-                assert conn
-            except Exception:
+                user_ref = self._get_user(session, user_id)
+            except exception.UserNotFound:
                 raise AssertionError('Invalid user / password')
+
+            # if the user_ref has a password, it's from the SQL backend and
+            # we can just check if it coincides with the one we got
+            conn = None
+            try:
+                assert utils.check_password(password, user_ref.password)
+            except TypeError:
+                raise AssertionError('Invalid user / password')
+            except (KeyError, AssertionError):  # if it doesn't have a password, it must be LDAP
+                try:
+                    user_name = user_ref['name']
+                    # get_connection does a bind for us which checks the password
+                    conn = self.user.get_connection(self.user._id_to_dn(user_name),
+                                                    password, end_user_auth=True)
+                    assert conn
+                except Exception:
+                    raise AssertionError('Invalid user / password')
+                else:
+                    LOG.debug("Authenticated user with LDAP.")
+                    self.domain_aware = False
+                finally:
+                    if conn:
+                        conn.unbind_s()
             else:
-                LOG.debug("Authenticated user with LDAP.")
-                self.domain_aware = False
-            finally:
-                if conn:
-                    conn.unbind_s()
-        else:
-            LOG.debug("Authenticated user with SQL.")
-        return identity.filter_user(user_ref.to_dict())
+                LOG.debug("Authenticated user with SQL.")
+            return identity.filter_user(user_ref.to_dict())
 
     def is_domain_aware(self):
         # XXX we only need domain_aware to be False when authenticating
@@ -108,14 +108,14 @@ class Identity(sql_ident.Identity):
 
     def get_user(self, user_id):
         LOG.debug("Called get_user %s" % user_id)
-        session = sql.get_session()
-        user = self._get_user(session, user_id)
-        try:
-            user = user.to_dict()
-        except AttributeError:
-            # LDAP Users are already dicts which is fine
-            pass
-        return identity.filter_user(user)
+        with sql.session_for_read() as session:
+            user = self._get_user(session, user_id)
+            try:
+                user = user.to_dict()
+            except AttributeError:
+                # LDAP Users are already dicts which is fine
+                pass
+            return identity.filter_user(user)
 
     def get_user_by_name(self, user_name, domain_id):
         LOG.debug("Called get_user_by_name %s, %s" % (user_name, domain_id))
